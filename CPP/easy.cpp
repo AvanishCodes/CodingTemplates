da43b55152429519226b680c10e446b7d6d91b0f
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-01-20 12:25:36 +0530

    Added template to easy questions with N test cases

---
+++ CPP/easy.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void testcase(){
+    
+    return;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+        testcase();
+    return 0;
+}
---
+++ Python/easy.py
@@ -0,0 +1,7 @@
+def testCase():
+    return
+
+if __name__ == "__main__":
+    n = int(input())
+    for i in range(n):
+        testCase()
