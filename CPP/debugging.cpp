adc46ae4ea8e20fc9d544bc9061a746e1adb01ec
AvanishCodes <avanishcodes@gmail.com>
2021-06-01 12:54:59 +0530

    Add a debugger

---
+++ CPP/debugging.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define error(args...)                       \
+  {                                          \
+    string _s = #args;                       \
+    replace(_s.begin(), _s.end(), ',', ' '); \
+    stringstream _ss(_s);                    \
+    istream_iterator<string> _it(_ss);       \
+    err(_it, args);                          \
+  }
+
+void err(istream_iterator<string> it)
+{
+}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+  cerr << *it << " = " << a << endl;
+  err(++it, args...);
+}
+
+int main()
+{
+  int a = 4, b = 8, c = 9;
+  error(a, b, c);
+}
