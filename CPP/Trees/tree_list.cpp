75b80238ce3f4908a2b873d26a9ee0b3ddcf8776
AvanishCodes <avanishcodes@gmail.com>
2021-06-17 10:35:10 +0530

    Representation of trees

---
+++ CPP/Trees/tree_list.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define max_nodes 10000
+
+vector<int> arr[max_nodes];
+
+int main()
+{
+  int n, a, b; //  n = number of vertices;
+  // Set the connected nodes
+  // If the problem assumes that each node is connected to itself
+  // Tree will always have n-1 edges
+  for (int i = 1; i < n; i++)
+    arr[i][i] = 1;
+  for (int i = 0; i < n; i++)
+    cin >> a >> b, arr[a].push_back(a), arr[b].push_back(a);
+}
